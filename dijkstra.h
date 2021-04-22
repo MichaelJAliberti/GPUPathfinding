217590e3d74639ff23ed1c65df93cdae8dd65ea9
Dmitry Zimin <yrtimd@bu.edu>
2021-04-22 14:03:02 -0400

    dijkstra v0.0.2

--- dijkstra.h
+++ dijkstra.h
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-void dijkstra(string grid_name, int src, int dst)
+void dijkstra(string grid_name)
 {
 	
-	grid map(grid_name);
+	Grid map(grid_name);
 	
 	int side_size = map.size;
 	int size = side_size * side_size;
@@ -19,67 +19,104 @@ void dijkstra(string grid_name, int src, int dst)
 	bool sptSet[size];
 	
 	int pred[size];
-	int path[size*map.numagents];
+	int path[size*map.num_agents];
 	int preditor;
 	int counter;
 	int src;
+	int dst;
 	
-	agent iter;
+	Agent iter;
 	
+	cout << "Passed: Initialization" << endl;
+	cout << map.num_agents << endl;
 	
-	
-	for(int i = 0; i < map.numagents; i++)
-	{
-		for (int i = 0; i < size; i++)
+		for(int i = 0; i < map.num_agents; i++)
 		{
-			dist[i] = INF;
-			sptSet[i] = false;
-			pred[i] = -1;
-		}
-		
-		iter = map.agents[i];
-		
-		src = iter.sx + iter.sy;
-		
-		pred[src] = src;
-		dist[src] = 0;
-		int min = INF;
-		int u;
-		for (int count = 0; count < V - 1; count++) {
-
+			for (int i = 0; i < size; i++)
+			{
+				dist[i] = INF;
+				sptSet[i] = false;
+				pred[i] = -1;
+			}
 			
-			min = INF;
-			for (int v = 0; v < size; v++)
-				if (sptSet[v] == false && dist[v] <= min)
-				{
-					min = dist[v];
-					u = v;
-				}
-		  
-			sptSet[u] = true;
-	  
-			for (int v = 0; v < size; v++)
-	  
-				if (!sptSet[v] && map.matrix[u*side_size+v] != -1 && dist[u] != INF
-					&& dist[u] + map.matrix[u*side_size+v] < dist[v])
-					{
-					dist[v] = dist[u] + map.matrix[u*side_size+v];
-					pred[u] = v;
-					}
-		}
-		
-		preditor = dst;
-		path[i*size] = dst;
-		counter = i*size+1;
-		while (dst != src)
-		{
-			path[counter] = pred[preditor];
-			preditor = pred[preditor];
-			counter++;
-		}
+			cout << "Passed: Initialization variables per Agent" << endl;
+			
+			iter = map.agents[i];
+			cout << "Passed: Agent Initialization" << endl;
+			
+			src = iter.sx + iter.sy;
+			dst = iter.dx + iter.dy;
+			
+			cout << "Passed: Agent Setup" << endl;
+			
+			pred[src] = src;
+			dist[src] = 0;
+			int min = INF;
+			int u;
+			
+			cout << "Passed: Load data per Agent" << endl;
+			
+			for (int count = 0; count < size - 1; count++) {
 
-	}	
+				
+				min = INF;
+				for (int v = 0; v < size; v++)
+					if (sptSet[v] == false && dist[v] <= min)
+					{
+						min = dist[v];
+						u = v;
+					}
+			  
+				sptSet[u] = true;
+				
+				cout << "Passed: Find Min" << endl;
+		  
+				//for (int v = 0; v < size; v++) // map.matrix[(u / side_size)*side_size + (u % side_size)] map.matrix[(u / side_size)*side_size + (u % side_size)]
+					int v = u + 1;
+					if (!sptSet[v] && !(v % side_size == 0) && map.matrix[v] && dist[u] != INF 
+						&& dist[u] + map.matrix[u*side_size+v] < dist[v])
+						{
+						dist[v] = dist[u] + map.matrix[u*side_size+v];
+						pred[u] = v;
+						}
+					v = u - 1;
+					if (!sptSet[v] && !(v % side_size == side_size - 1) && map.matrix[v] && dist[u] != INF 
+						&& dist[u] + map.matrix[u*side_size+v] < dist[v])
+						{
+						dist[v] = dist[u] + map.matrix[u*side_size+v];
+						pred[u] = v;
+						}
+					v = u + side_size;
+					if (!sptSet[v] && !(v / side_size == side_size - 1) && map.matrix[v] && dist[u] != INF 
+						&& dist[u] + map.matrix[u*side_size+v] < dist[v])
+						{
+						dist[v] = dist[u] + map.matrix[u*side_size+v];
+						pred[u] = v;
+						}
+					v = u - side_size;
+					if (!sptSet[v] && !(v < 0) && map.matrix[v] && dist[u] != INF 
+						&& dist[u] + map.matrix[u*side_size+v] < dist[v])
+						{
+						dist[v] = dist[u] + map.matrix[u*side_size+v];
+						pred[u] = v;
+						}
+			}
+			
+			preditor = dst;
+			path[i*size] = dst;
+			counter = i*size+1;
+			while (dst != src)
+			{
+				path[counter] = pred[preditor];
+				preditor = pred[preditor];
+				counter++;
+				cout << path[counter] << endl;
+			}
+			cout << endl;
+
+		}	
 	
+
 	
 
 } 
