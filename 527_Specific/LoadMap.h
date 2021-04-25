b82dde8b37aa21b47ec2bfb24d2e427c6a64f4df
unknown <mjali@bu.edu>
2021-04-25 14:53:55 -0400

    added repo fro 527-specific work

---
+++ 527_Specific/LoadMap.h
@@ -0,0 +1,162 @@
+#ifndef MAKE_GRID
+#define MAKE_GRID
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**************************************************************
+						AGENT CLASS
+**************************************************************/
+
+typedef double data_t;
+
+typedef struct{
+	data_t x;
+	data_t y;
+} point, point_ptr;
+
+typedef struct{
+	point start;
+	point dest;
+	char* name;
+} agent, *agent_ptr;
+
+/**************************************************************
+						GRID CLASS
+**************************************************************/
+
+class Grid{
+public:
+	// variables
+	int num_agents;
+	vector<Agent> agents;
+	int size;
+	int* matrix;
+
+	// constructors
+	Grid(); // default
+	Grid(string filename);
+
+	// member functions
+	int PrintGrid();
+};
+
+Grid::Grid(){
+	num_agents = 0;
+	size = 0;
+	matrix = (int*) calloc(size * size, sizeof(int));
+}
+
+Grid::Grid(string filename){
+	// Reads from YAML file to initialize grid
+	int i, j, mult;
+	int x, y, aso_x, aso_y, ades_x, ades_y;
+	string line, token, aname;
+	fstream infile;
+
+	num_agents = 0;
+
+	infile.open(filename);
+	if (!infile){
+		cout << "Cannot open file" << endl;
+		exit(1);
+	}
+
+	// Line Parser/load data
+	try{
+		getline(infile, line); // agents
+
+		// handle agent info
+		while (getline(infile, line)){
+			if (line.compare(0, 4, "map:") == 0)
+				break;
+
+			// increment agent count
+			num_agents++;
+			
+			// read in goal
+			stringstream s1(line);
+			getline(s1, token, '['); // remove padding
+			getline(s1, token, ','); // first digit
+			ades_x = stoi(token);
+			getline(s1, token, ']'); // second digit
+			ades_y = stoi(token);
+
+			// read in name
+			getline(infile, line);
+			stringstream s2(line);
+			getline(s2, token, ':'); // remove padding
+			getline(s2, token, ' '); // remove padding
+			getline(s2, token); // name
+			aname = token;
+
+			// read in start
+			getline(infile, line);
+			stringstream s3(line);
+			getline(s3, token, '['); // remove padding
+			getline(s3, token, ','); // first digit
+			aso_x = stoi(token);
+			getline(s3, token, ']'); // second digit
+			aso_y = stoi(token);
+
+			// initialize agent
+			Agent bot(aso_x, aso_y, ades_x, ades_y, aname);
+			agents.push_back(bot);
+		}
+
+		// get dimensions
+		getline(infile, line);
+		stringstream ss(line);
+		getline(ss, token, '['); // remove padding
+		getline(ss, token, ','); // first digit
+		size = stoi(token);
+		getline(ss, token, ']'); // second digit
+
+		// initialize matrix w/ 0s
+		matrix = (int*) calloc(size * size, sizeof(int));
+		for (i = 0; i < size; i++){
+			mult = i*size;
+			for (j = 0; j < size; j++){
+				matrix[mult + j] = 0;
+			}
+		}
+
+		getline(infile, line); // remove padding
+
+		// Load obstacle info into matrix
+		while (getline(infile, line)){
+			stringstream ss(line);
+			getline(ss, token, '['); // remove padding
+			getline(ss, token, ','); // first digit
+			x = stoi(token);
+			getline(ss, token, ']'); // second digit
+			y = stoi(token);
+
+			matrix[x + y*size] = -1; // obstacle to -1
+		}
+	}
+	catch (...){
+		cout << "Cannot parse file..." << endl << "SHUTTING DOWN" << endl;
+		infile.close();
+		exit(1);
+	}
+
+	infile.close();
+}
+
+int Grid::PrintGrid(){
+	int i, j, mult;
+
+	for (i = 0; i < size; i++){
+		mult = i*size;
+		for (j = 0; j < size; j++){
+			cout << matrix[mult + j] << ",\t";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
+#endif
