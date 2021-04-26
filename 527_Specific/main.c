03c623222049bc8a16d2b4d0309827bda87950e9
isaacfc4 <isaacfc4@bu.edu>
2021-04-25 23:28:53 -0400

    ed

--- 527_Specific/main.c
+++ 527_Specific/main.c
@@ -11,12 +11,13 @@
 void diffuse(grid* g);
 int checkDiffusion(grid* g);
 void traversePath(grid* g);
+float getNeighborSum(grid* g, int ax, int ay);
 
 /*****************************************************************************/
 int main(int argc, char *argv[])
 {
     /* declare and initialize the array */
-    grid* myGrid = LoadGrid(argv[0]); //this creates the grid to use
+    grid* myGrid = LoadGrid("map_8by8_obst12_agents1_ex0.yaml"); //this creates the grid to use
 
     /*this runs the entire diffusion process and will run until the graph is fully diffused.*/
     PrintGrid(myGrid);
@@ -39,12 +40,15 @@ void diffuse(grid* g)
   data_t *obstacles = g->obs_matrix;
   double newD;
   int destX = g->dx, destY = g->dy;
+  float sum;
 
   while (!checkDiffusion(g)) {
     data[destX*rowlen+destY] = rowlen;
-    for (i = 1; i < rowlen-1; i++) {
-      for (j = 1; j < rowlen-1; j++) {
-        newD = .25 * (data[(i-1)*rowlen+j] + data[(i+1)*rowlen+j] + data[i*rowlen+j+1] + data[i*rowlen+j-1]) * obstacles[destX*rowlen+destY];
+    for (i = 0; i < rowlen; i++) {
+      for (j = 0; j < rowlen; j++) {
+        //newD = .25 * (data[(i-1)*rowlen+j] + data[(i+1)*rowlen+j] + data[i*rowlen+j+1] + data[i*rowlen+j-1]) * obstacles[destX*rowlen+destY];
+        sum = getNeighborSum(g, i, j);
+        newD = .25 * sum * obstacles[destX*rowlen+destY];
         data[i*rowlen+j] = newD;
       }
     }
@@ -53,13 +57,14 @@ void diffuse(grid* g)
 
 int checkDiffusion(grid* g){
     int i,j;
+    int debug1=1,debug2=2,debug3=3;
     int rowlen = g->size;
     data_t *data = g->diff_matrix;
     data_t *obstacles = g->obs_matrix;
-
+    PrintGrid(g);
     for(i = 0; i < rowlen; i++){
         for(j = 0; j < rowlen; j++){
-            if(obstacles[i*rowlen+j] || (data[i*rowlen+j] <= 0)){return 0;}
+            if(!obstacles[i*rowlen+j] && (data[i*rowlen+j] <= 0)){return 0;}
         }
     }
     return 1;
@@ -96,4 +101,16 @@ void traversePath(grid* g){
     //fp << "Path Length: " << pathLength << endl;
     fprintf(fp, "Path Length: %d", pathLength);
     fclose(fp);
+}
+
+float getNeighborSum(grid* g, int ax, int ay){
+    float runsum = 0;
+    int rowlen = g->size;
+    data_t *data = g->diff_matrix;
+    if(ax > 0){runsum += data[ax*rowlen+ay-1]; printf("Adding top neighbor %f to runsum\n", data[ax*rowlen+ay-1]);}
+    if(ax < rowlen){runsum += data[ax*rowlen+ay+1]; printf("Adding bottom neighbor %f to runsum\n", data[ax*rowlen+ay+1]);}
+    if(ay > 0){runsum += data[(ax-1)*rowlen+ay];printf("Adding left neighbor %f to runsum\n", data[(ax-1)*rowlen+ay]);}
+    if(ay < rowlen){runsum += data[(ax+1)*rowlen+ay]; printf("Adding right neighbor %f to runsum\n", data[(ax+1)*rowlen+ay]);}
+    printf("[%d,%d] Runsum: %2f\n\n",ax,ay,runsum);
+    return runsum;
 }
